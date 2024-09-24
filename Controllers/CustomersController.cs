cd12d319bbba99ebdee1962038783352f275e719
Amal Aden <amal.aden@student.careeria.fi>
2024-09-24 18:28:40 +0300

    Httpget  all custumers try/catch koodi Httget id haku metodi

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -15,9 +15,40 @@ namespace NortWindOriginalRestApi1.Controllers
         [HttpGet]
         public ActionResult GetAllCustomers() 
         {
-             var asiakkaat = db.Customers.ToList();
-            return Ok(asiakkaat);
+            try
+            { 
+                var asiakkaat = db.Customers.ToList();
+                return Ok(asiakkaat);
+
+            }
+            catch (Exception ex)
+            { 
+                return BadRequest("Tapahtui virhe. Lue lisää: " + ex.InnerException);
+            
+            }
+            
         }
+
+
+
+        //luodaa metodi get määritellyllä ID  joka Hakee kaikki asiakkaat
+        [HttpGet("{id}")]
+       
+        public ActionResult GetOneCustomersById(string id)
+        {
+            var asiakas = db.Customers.Find(id);
+            if (asiakas != null ) 
+            { 
+                return Ok(asiakas);
+            }
+            else
+            {
+                //return BadRequest("Asikasta id:llä " + "ei löydy");
+                return BadRequest($"Asiakasta id:llä {id } ei löydy");// string interpolation
+            }
+            
+        }
+
     }
        
 }
