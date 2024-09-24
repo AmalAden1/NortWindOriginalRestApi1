6a220f5d7c92340fe3f6bd2dd59bd473a20c84a5
Amal Aden <amal.aden@student.careeria.fi>
2024-09-24 17:58:26 +0300

    Add project files.

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NortWindOriginalRestApi1.Models;
+
+
+namespace NortWindOriginalRestApi1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        NorthwindOriginalContext db = new NorthwindOriginalContext();
+
+        //luodaa metodi get joka Hakee kaikki asiakkaat
+        [HttpGet]
+        public ActionResult GetAllCustomers() 
+        {
+             var asiakkaat = db.Customers.ToList();
+            return Ok(asiakkaat);
+        }
+    }
+       
+}
---
+++ Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NortWindOriginalRestApi1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        //get metodi julkinen ja palauttaa hello word string
+        [HttpGet]
+        public string Get()
+        {
+            return ("Hello Word");
+        }
+    }
+}
